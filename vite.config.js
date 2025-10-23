d00de59bea48b5a0cde228a76802eb39a8b3e845
VarissIndia <varisscompany@gmail.com>
2025-10-23 18:53:40 +0530

    Resize images to improve Performance

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: '/'
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    legacy({
+      targets: ["defaults", "Android >= 6", "iOS >= 10"],
+      // Safely adds ES5 fallbacks for older browsers
+    }),
+  ],
+  base: "/",
+});
